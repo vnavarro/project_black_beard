8253592662adfff3e502af662cc508361c1a91d4
Vitor Navarro <vitor.navarro87@gmail.com>
2014-01-16 00:00:32 -0200

    Improve code readability

--- Assets/Scripts/ShipSeek.cs
+++ Assets/Scripts/ShipSeek.cs
@@ -5,15 +5,18 @@ public class ShipSeek : MonoBehaviour {
 
 	public Vector2 velocity;
 	public Vector2 destination;
-	public float velocityLimit;
+	public float velocityLimit = 3;
 	private Vector2 position;
 
 	public bool followMouse;
 
+	public float mass = 20;
+	public float maxSteeringForce = 0.4f;
+
 	// Use this for initialization
 	void Start () {
 		position = new Vector2 ();
-		this.velocity = new Vector2 ();
+		this.velocity = new Vector2 (0,0);
 	}
 
 	// Update is called once per frame
@@ -29,20 +32,34 @@ public class ShipSeek : MonoBehaviour {
 		position.x = this.transform.localPosition.x;
 		position.y = this.transform.localPosition.y;
 
+		Vector2 steering = Seek (this.destination);
+		steering = this.TruncateVector2 (steering, maxSteeringForce);
+		steering /= mass;
 
-		//This this.velocity = normalize(target - position) * max_velocity
-		// is the same as the lines bellow 
-		this.velocity = this.destination - position;
+		velocity = this.TruncateVector2 (this.velocity + steering, this.velocityLimit);
 
-//		if (this.velocity.magnitude > this.velocityLimit) {
-
-		this.velocity = this.velocity.normalized * this.velocityLimit;
-
-//		}
-
-		position = position + this.velocity;
+		position += this.velocity;
 
 		Vector3 start = this.transform.position;
 		this.transform.localPosition = Vector3.Lerp(start, this.position, Time.deltaTime);
+	}
+
+	Vector2 Seek(Vector2 destination){
+		//		Vector2 desiredVelocity = this.destination - position;
+		//		Vector2 steering = desiredVelocity - this.velocity;
+
+		Vector2 desired = destination - this.position;
+		desired.Normalize();
+		desired = desired * velocityLimit;
+//		desired = this.TruncateVector2 (desired, velocityLimit);
+
+		return desired - this.velocity;
+	}
+
+	Vector2 TruncateVector2(Vector2 force,float forceLimit){
+		float truncate = forceLimit / force.magnitude;
+		truncate = truncate < 1.0f ? 1.0f : truncate;
+				
+		return force.normalized * forceLimit;
 	}
 }
